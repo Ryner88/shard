a8e990698d6e991f1ca6df7032b97c757939f2b9
Samuel Badejo <samuelbadejo928@gmail.com>
2025-09-16 00:19:54 -0400

    feat(map): /map JSON endpoint; chore(db): idempotent indexes; docs: quick start

--- lib/shard_web/router.ex
+++ lib/shard_web/router.ex
@@ -16,27 +16,18 @@ defmodule ShardWeb.Router do
 
   scope "/", ShardWeb do
     pipe_through :browser
+    get "/map", MapController, :index
 
     get "/", PageController, :home
+    # NEW
+    live "/play", PlayLive
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", ShardWeb do
-  #   pipe_through :api
-  # end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:shard, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
       pipe_through :browser
-
       live_dashboard "/dashboard", metrics: ShardWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
